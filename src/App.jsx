752f0d5615796528741cb328b8688c8cdc4e2026
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-07 10:53:52 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import Container from './components/Container';
 import { quotes, quoteOfTheDay, spinner } from './state/reducers';
+import * as types from './state/actionTypes';
 
 // create custom middleware to save 'userToken' to local storage on LOGIN_SUCCESS
-const customMiddleware = store => next => action => {
-  // do something with the action here, and then don't forget to call next!
+const customMiddlewareToSaveUserToke = store => next => action => {
+  console.log(store);
+  if (action.type === types.LOGIN_SUCCESS) {
+    localStorage.setItem('userToken', action.payload);
+  }
   next(action);
 };
 
@@ -22,7 +26,7 @@ const store = createStore(
   rootReducer,
   {},
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, customMiddlewareToSaveUserToke),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
