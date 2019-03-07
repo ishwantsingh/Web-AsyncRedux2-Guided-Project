9ca475c9a136d34326b8472e6726a06382e15395
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-07 11:00:40 +0100

    wip

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,8 +4,14 @@ export default class Login extends React.Component {
   render() {
     return (
       <div>
-
+        {/* username input */}
+        {/* password input */}
+        {/* UNCONTROLLED -> USE REFS */}
+        {/* LOGIN BUTTON invokes this.props.login */}
+        {/* lOGOUT BUTTON invokes clear local storage */}
       </div>
     );
   }
 }
+
+// connect it so we get the login action creator
