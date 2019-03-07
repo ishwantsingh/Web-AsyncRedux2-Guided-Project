aa179de091badb6c3ea0908278564f0a04977d4a
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-07 11:05:47 +0100

    wip

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { login } from '../state/actionCreators';
 
-export default class Login extends React.Component {
+
+export class Login extends React.Component {
+  userRef = React.createRef()
+
+  passRef = React.createRef()
+
+  onLogin = () => {
+    const username = this.userRef.current.value;
+    const password = this.passRef.current.value;
+    
+    this.props.login({ username, password })
+  }
+
   render() {
     return (
       <div>
-        {/* username input */}
-        {/* password input */}
-        {/* UNCONTROLLED -> USE REFS */}
-        {/* LOGIN BUTTON invokes this.props.login */}
-        {/* lOGOUT BUTTON invokes clear local storage */}
+        <div>username <input type="text" ref={this.userRef} /></div>
+        <div>password <input type="text" ref={this.passRef} /></div>
+
+        <button onClick={}>Log in</button>
+        <button onClick={() => localStorage.clear()}>Log out</button>
       </div>
     );
   }
 }
 
+export default connect(st => st, { login })(Login);
 // connect it so we get the login action creator
