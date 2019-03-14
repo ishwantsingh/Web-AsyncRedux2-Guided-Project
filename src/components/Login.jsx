fc3ad9c2d18ac3997a9928cb9ed04a15d1728ac1
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-14 09:36:42 +0100

    typo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,7 +20,7 @@ export default class Login extends React.Component {
         <div>password <input type="text" ref={this.passRef} /></div>
 
         <button onClick={this.onLogin}>Log in</button>
-        {/* Create a Log Out button that flushes 'userKey' from local storage */}
+        {/* Create a Log Out button that flushes 'userToken' from local storage */}
       </div>
     );
   }
